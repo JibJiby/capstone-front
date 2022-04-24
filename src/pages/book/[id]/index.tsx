c60f0c6559f0efda093dea46ed4b2a4e97a8925a
jibjiby <my01sun@gmail.com>
2022-04-24 17:23:25 +0900

    style: 도서 상세 페이지 스타일링

--- src/pages/book/[id]/index.tsx
+++ src/pages/book/[id]/index.tsx
@@ -1,6 +1,26 @@
 import { useRouter } from 'next/router'
 import { bookImage } from '@data/fake-book'
 import AppLayout from '@components/AppLayout'
+import Image from 'next/image'
+import styled from '@emotion/styled'
+
+const LinkButton = styled.button`
+    cursor: pointer;
+    height: 40px;
+    width: 100px;
+    font-weight: bold;
+    background-color: #212529;
+    border: none;
+    border-radius: 6px;
+    color: white;
+    font-size: 18px;
+
+    transition: 0.3s;
+
+    &:hover {
+        opacity: 0.8;
+    }
+`
 
 const Book = () => {
     const router = useRouter()
@@ -9,11 +29,50 @@ const Book = () => {
     return (
         <AppLayout>
             <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
-                <div style={{ marginBottom: '30px', userSelect: 'none' }}>
-                    <h1>상세 정보</h1>
+                <div
+                    style={{
+                        marginBottom: '30px',
+                        marginTop: '40px',
+                        backgroundColor: '#e9ecef',
+                        display: 'flex',
+                        width: '70%',
+                        borderRadius: '8px',
+                    }}
+                >
+                    <div style={{ padding: '10px 25px', userSelect: 'none' }}>
+                        <img src={id && !Array.isArray(id) ? bookImage[parseInt(id)] : undefined} />
+                    </div>
+                    <div style={{ marginTop: '20px' }}>
+                        <div style={{ marginTop: '10px', fontSize: '24px', fontWeight: 'bold' }}>도서 제목</div>
+                        <div style={{ marginTop: '14px', fontSize: '18px' }}>작가 / 출판사</div>
+                        <div style={{ marginTop: '14px', fontSize: '11pt', height: '70px' }}>줄거리 ...</div>
+                        <div style={{ marginTop: '14px', display: 'flex', gap: '10px' }}>
+                            <Image src="/rating-star.png" width={40} height={40} />
+                            <Image src="/rating-star.png" width={40} height={40} />
+                            <Image src="/rating-star.png" width={40} height={40} />
+                            <Image src="/rating-star.png" width={40} height={40} />
+                            <Image src="/rating-star.png" width={40} height={40} />
+                        </div>
+                    </div>
                 </div>
-                <div>
-                    <img src={id && !Array.isArray(id) ? bookImage[parseInt(id)] : undefined} />
+                <div
+                    style={{
+                        display: 'flex',
+                        width: '70%',
+                        justifyContent: 'end',
+                        userSelect: 'none',
+                        gap: '20px',
+                        alignItems: 'center',
+                    }}
+                >
+                    <div style={{ display: 'inline-block', cursor: 'pointer' }}>
+                        <Image src="/save-icon.png" width={20} height={20} />
+                    </div>
+                    <div style={{ display: 'inline-flex', alignItems: 'center', gap: '5px' }}>
+                        <span style={{ fontWeight: 'bold', fontSize: '18px', letterSpacing: '.7px' }}>21,000</span>
+                        <span style={{ fontSize: '24px' }}> 원</span>
+                    </div>
+                    <LinkButton>구매 링크</LinkButton>
                 </div>
             </div>
         </AppLayout>
