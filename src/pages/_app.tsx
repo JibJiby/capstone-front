3075c52b4c0b822d36e566012a71a9f95c90f5b9
jibjiby <my01sun@gmail.com>
2022-06-09 16:40:37 +0900

    chore

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,7 @@ import { globalStyles } from '@styles/globals'
 
 // import '@styles/antd.less'
 // https://stackoverflow.com/questions/60398647/ant-design-page-reloading-css-rendering-delay
-// import 'antd/dist/antd.css'
+import 'antd/dist/antd.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
     const queryClientRef = useRef<QueryClient>()
