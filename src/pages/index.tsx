07228468d8379bc932c8d61e4309060d6e34e836
jibjiby <my01sun@gmail.com>
2022-06-11 01:36:28 +0900

    update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -154,9 +154,12 @@ export const getServerSideProps = async (context: GetServerSidePropsContext) => 
                 permanent: false,
             },
         }
-    } catch (err) {
+    } catch (err: any) {
         // 비로그인 상태라면 이대로.
-
+        let statusCode = err?.response?.status
+        console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-')
+        console.log(err?.response)
+        console.log('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-')
         console.error(err)
 
         return {
@@ -165,10 +168,6 @@ export const getServerSideProps = async (context: GetServerSidePropsContext) => 
                 permanent: false,
             },
         }
-    }
-
-    return {
-        props: {},
     }
 }
 
